1988f8b7a6f4a7b063789d72e976a1d89c43270f
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2024-01-26 11:32:48 -0500

    updated with my own informations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 import { CommandMenu } from "@/components/command-menu";
-import { Metadata } from "next";
-import { Section } from "@/components/ui/section";
-import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { RESUME_DATA } from "@/data/resume-data";
 import { ProjectCard } from "@/components/project-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Section } from "@/components/ui/section";
+import { RESUME_DATA } from "@/data/resume-data";
+import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
+    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
@@ -34,60 +34,50 @@ export default function Page() {
                 {RESUME_DATA.location}
               </a>
             </p>
-            <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-              {RESUME_DATA.contact.email ? (
-                <Button
-                  className="h-8 w-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                    <MailIcon className="h-4 w-4" />
-                  </a>
-                </Button>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <Button
-                  className="h-8 w-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                    <PhoneIcon className="h-4 w-4" />
-                  </a>
-                </Button>
-              ) : null}
-              {RESUME_DATA.contact.social.map((social) => (
-                <Button
-                  key={social.name}
-                  className="h-8 w-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={social.url}>
-                    <social.icon className="h-4 w-4" />
-                  </a>
-                </Button>
-              ))}
-            </div>
-            <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
-              {RESUME_DATA.contact.email ? (
+          </div>
+          <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
+            {RESUME_DATA.contact.email ? (
+              <Button className="h-8 w-8" variant="outline" size="icon" asChild>
                 <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                  <span className="underline">{RESUME_DATA.contact.email}</span>
+                  <MailIcon className="h-4 w-4" />
                 </a>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
+              </Button>
+            ) : null}
+            {RESUME_DATA.contact.tel ? (
+              <Button className="h-8 w-8" variant="outline" size="icon" asChild>
                 <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className="underline">{RESUME_DATA.contact.tel}</span>
+                  <PhoneIcon className="h-4 w-4" />
                 </a>
-              ) : null}
-            </div>
+              </Button>
+            ) : null}
+            {RESUME_DATA.contact.social.map((social) => (
+              <Button
+                key={social.name}
+                className="h-8 w-8"
+                variant="outline"
+                size="icon"
+                asChild
+              >
+                <a href={social.url}>
+                  <social.icon className="h-4 w-4" />
+                </a>
+              </Button>
+            ))}
+          </div>
+          <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
+            {RESUME_DATA.contact.email ? (
+              <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                <span className="underline">{RESUME_DATA.contact.email}</span>
+              </a>
+            ) : null}
+            {RESUME_DATA.contact.tel ? (
+              <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                <span className="underline">{RESUME_DATA.contact.tel}</span>
+              </a>
+            ) : null}
           </div>
 
-          <Avatar className="h-28 w-28">
+          <Avatar className="hidden h-28 w-28">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
@@ -106,10 +96,13 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
-                      <a className="hover:underline" href={work.link}>
-                        {work.company}
-                      </a>
-
+                      {work.link ? (
+                        <a className="hover:underline" href={work.link}>
+                          {work.company}
+                        </a>
+                      ) : (
+                        work.company
+                      )}
                       <span className="inline-flex gap-x-1">
                         {work.badges.map((badge) => (
                           <Badge
